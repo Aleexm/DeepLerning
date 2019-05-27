d5e9b73208e2d1374bdab29b53422566232607bc
Alex Mandersloot <alexmandersloot@hotmail.nl>
2019-05-27 10:47:54 +0200

    folders per output, generic analysis function

--- analysis.py
+++ analysis.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 import analysisPlotFunctions as plf
 
-# Add correct path to csv's here.
-# Also, savefig is currently hardcoded to my desktop. You should add a valid path here if you want to save figs (which it does by default)
-# dfGer = pd.read_csv("Ger/VGG16/VGG16_results.csv")
 dfEur = pd.read_csv("output/VGG16ADDEUR/VGG16ADDEUR_results.csv")
-dfAug = pd.read_csv("C:/Users/alexm/Desktop/BlurAug/VGG16ADDBLUR_results.csv")
-label = 'Pred_Occl_25_Label' #Class ID you want to inspect. Examples are Pred_Label, Orig_Label, Pred_Blurred_x_Label, Pred_Bright_x_Label,
+
+#IMPORTANT: ADD THE CORRECT CSV YOU'RE INSPECTING HERE
+dfAug = pd.read_csv("output/VGG16ADDBLUR/VGG16ADDBLUR_results.csv")
+label = 'Pred_Blurred_15_Label' #Class ID you want to inspect. Examples are Pred_Label, Orig_Label, Pred_Blurred_x_Label, Pred_Bright_x_Label,
 # Pred_Dark_x_Label and Pred_Occl_x_Label
 id = 11 # Current class object that you want to inspect misclassifcations
 # Plots the class accuracy for this label, i.e. the accuracy per class on the occluded_25 dataset.
@@ -22,4 +21,4 @@ print(dfEur.loc[(dfEur['Orig_Label'] == id) & (dfEur[label] != id)][['Orig_Label
 print(dfAug.loc[(dfAug['Orig_Label'] == id) & (dfAug[label] != id)][['Orig_Label', label, 'Pred_Label']])
 # Prints the misclassifications as, for this class id. (Found this easier than looking at the confmat)
 print(dfEur.loc[dfEur['Orig_Label'] == id][label].value_counts())
-print(dfAug.loc[dfGer['Orig_Label'] == id][label].value_counts())
+print(dfAug.loc[dfAug['Orig_Label'] == id][label].value_counts())
--- analysisPlotFunctions.py
+++ analysisPlotFunctions.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
 def plotClassAccuracy(df1, df2, label):
-    "Plot Accuracy per class for given test-set (e.g. Pred_Blurred_15_Label), for both German (df1) and Eur (df2) datasets,"
-    "as well as the original label and original test-set predictions without augmentation for comparison"
+    '''Plot Accuracy per class for given test-set (e.g. Pred_Blurred_15_Label), for both German (df1) and Eur (df2) datasets,
+    as well as the original label and original test-set predictions without augmentation for comparison
+    '''
     numClasses = 43
     priorClass1 = [len(df1.loc[df1['Orig_Label'] == i]) for i in range(43)]
     priorClass2 = [len(df1.loc[df2['Orig_Label'] == i]) for i in range(43)]
@@ -26,10 +27,10 @@ def plotClassAccuracy(df1, df2, label):
             origAccEur[key] += 1/priorClass2[key]
     figure(num=None, figsize=(20, 10), dpi=80, facecolor='w', edgecolor='k')
     w=0.2
-    plt.bar(np.arange(0,43,1)-1.5*w, origAccGer, width=w, label = "Orig Eur", color = 'blue')
-    plt.bar(np.arange(0,43,1)-0.5*w, origAccEur, width=w, label = "Orig Aug", color = 'red')
-    plt.bar(np.arange(0,43,1)+0.5*w, accuracies1, width=w, label = "{} Eur".format(label), color = 'orange')
-    plt.bar(np.arange(0,43,1)+1.5*w, accuracies2, width=w, label = "{} Aug".format(label), color = 'g')
+    plt.bar(np.arange(0,43,1)-1.5*w, origAccGer, width=w, label = "Orig Test Set added Eur", color = 'blue')
+    plt.bar(np.arange(0,43,1)-0.5*w, origAccEur, width=w, label = "Orig Test Set added Augmented Eur", color = 'red')
+    plt.bar(np.arange(0,43,1)+0.5*w, accuracies1, width=w, label = "Augmented Test Set added Eur", color = 'orange')
+    plt.bar(np.arange(0,43,1)+1.5*w, accuracies2, width=w, label = "Augmented Test Set augmented Eur", color = 'g')
     plt.xticks(np.arange(0,43,1))
     plt.xlabel("Class", size =14)
     plt.ylabel("Accuracy", size= 14)
@@ -41,8 +42,9 @@ def plotClassAccuracy(df1, df2, label):
     # print(accuracies[0])
 
 def plotBothFeat(df1, df2, feat):
-    "Plots count of prediction per class, for a given feature (e.g. Pred_Blurred_15_Label), as well as the"
-    "True Label, Original prediction for both German (df1) and European (df2)"
+    '''Plots count of prediction per class, for a given feature (e.g. Pred_Blurred_15_Label), as well as the
+    True Label, Original prediction for both German (df1) and European (df2)
+    '''
     numClasses = 43
     orig = np.zeros(numClasses)
     predOrigGer = np.zeros(numClasses)
@@ -75,9 +77,10 @@ def plotBothFeat(df1, df2, feat):
 
 
 def plotPredFeat(df, feat, lab1, lab2, lab3, levels):
-    "Not currently called from plots.py. I used this initially  for one dataframe."
-    "You can use this to plot 3 features (e.g. Pred_Blurred_15_Label, Pred_Blurred_10_Label and Pred_Blurred_5_Label)"
-    "for a single dataframe (e.g. German). Specify label (Blurred here), the 3 features (as above), as well as their levels [5,10,15] in this case"
+    '''Not currently called from plots.py. I used this initially  for one dataframe.
+    You can use this to plot 3 features (e.g. Pred_Blurred_15_Label, Pred_Blurred_10_Label and Pred_Blurred_5_Label)
+    for a single dataframe (e.g. German). Specify label (Blurred here), the 3 features (as above), as well as their levels [5,10,15] in this case
+    '''
     numClasses = 43
     orig = np.zeros(numClasses)
     pred = np.zeros(numClasses)
