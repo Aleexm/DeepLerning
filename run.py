031736a6821dd6c2bb21f2ca3550fc77a3546335
Simion-Constantinescu Andrei <andrei.simion.c@gmail.com>
2019-07-08 15:09:14 +0300

    Delete extra files

--- run.py
+++
@@ -1,9 +0,0 @@
-import csv
-import os
-
-RESULTS = [
-    ['apple','cherry','orange','pineapple','strawberry']
-]
-with open("output.csv",'a', newline = '') as resultFile:
-    wr = csv.writer(resultFile)
-    wr.writerows(RESULTS)
